a41496a9f5b82e263218330981a8d661704e55a7
Joe <ujbeszel@gmail.com>
2018-01-02 15:17:06 +0100

    Remove old hooks and add fields for admin menu

--- exopite-lightbox35/admin/class-exopite-lightbox35-admin.php
+++ exopite-lightbox35/admin/class-exopite-lightbox35-admin.php
@@ -89,59 +89,130 @@ class exopite_Lightbox35_Admin {
 	    );
 	}
 
-	 /**
-	 * Add settings action link to the plugins page.
-	 *
-	 * @since    1.0.0
-	 */
+    public function create_menu() {
 
-	public function add_action_links( $links ) {
-	    /*
-	    *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
-	    */
-	   $settings_link = array(
-	    '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
-	   );
-	   return array_merge(  $settings_link, $links );
+        $config = array(
+            'type'              => 'menu',                          // Required, menu or metabox
+            'id'                => $this->plugin_name,    // Required, meta box id, unique per page, to save: get_option( id )
+            'menu'              => 'plugins.php',                   // Required, sub page to your options page
+            'submenu'           => true,                            // Required for submenu
+            'title'             => 'Exopite LightBox35',               //The name of this page
+            'capability'        => 'manage_options',                // The capability needed to view the page
+            'plugin_basename'   =>  plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' ),
+            'tabbed'            => false,
+        );
 
-	}
+        $fields[] = array(
+            'name'   => 'first',
+            'fields' => array(
 
-	/**
-	 * Render the settings page for this plugin.
-	 *
-	 * @since    1.0.0
-	 */
+                array(
+                    'type'    => 'card',
+                    'content' => '<p>' . esc_attr__( 'Responsive Touch-enabled jQuery Image Lightbox Plugin.', 'exopite-lightbox35' ) . '</p>' .
+                        esc_attr__( 'Open all DIVs (required class "lightbox35-lightbox") and images (for images no class required!) on the page, which inside a link tag, except with "lightbox35-no-lightbox" class (this only required for images, div without the "lightbox35-lightbox" class will not listed automatically). Also if the link tag has the class "lightbox35-no-gallery", will not listed in the gallery, but opens individually.', 'exopite-lightbox35' ) . '</p>' .
+                        esc_attr__( 'Caption on the image, the program looking for the div inside in the link (A) tag with a class "lightbox35-lightbox-caption", if none, then looking for the image title attribute. With CSS you can customize the look and feel. You can use HTML inside div caption but because nested link are invalid, please place your link inside caption div like: [url="link" attribute="value"]name of the link[/url].', 'exopite-lightbox35' ) . '</p>' .
+                        esc_attr__( 'Navigate between images with the arrow keys, mouse wheel, click on image left or right side and with a swipe as well.', 'exopite-lightbox35' ) . '</p>' .
+                        esc_attr__( 'Responsive (working well on mobiles too).', 'exopite-lightbox35' ) . '</p>' .
+                        esc_attr__( 'Feel free to modify and share!', 'exopite-lightbox35' ) . '</p>',
+                    'header' => esc_attr__( 'Information', 'exopite-lightbox35' ),
+                ),
 
-	public function display_plugin_setup_page() {
-	    include_once( 'partials/exopite-lightbox35-admin-display.php' );
-	}
+                array(
+                    'id'      => 'single_image_only',
+                    'type'    => 'switcher',
+                    'title'   => esc_attr__( 'Single image mode', 'exopite-lightbox35' ),
+                    'default' => 'no',
+                ),
 
-	/**
-	 * Validate fields from admin area plugin settings form ('exopite-lazy-load-xt-admin-display.php')
-	 * @param  mixed $input as field form settings form
-	 * @return mixed as validated fields
-	 */
-	public function validate($input) {
-	    // All checkboxes inputs
-	    $valid = array();
+                array(
+                    'id'      => 'hide_caption',
+                    'type'    => 'switcher',
+                    'title'   => esc_attr__( 'Hide caption', 'exopite-lightbox35' ),
+                    'default' => 'no',
+                ),
 
-		$valid['single_image_only'] = (isset($input['single_image_only']) && !empty($input['single_image_only'])) ? 1 : 0;
-		$valid['hide_caption'] = (isset($input['hide_caption']) && !empty($input['hide_caption'])) ? 1 : 0;
-		$valid['hide_thumbnails'] = (isset($input['hide_thumbnails']) && !empty($input['hide_thumbnails'])) ? 1 : 0;
-		$valid['hide_open_in_new_window'] = (isset($input['hide_open_in_new_window']) && !empty($input['hide_open_in_new_window'])) ? 1 : 0;
-		$valid['hide_download'] = (isset($input['hide_download']) && !empty($input['hide_download'])) ? 1 : 0;
-	    $valid['style'] = (isset($input['style']) && !empty($input['style'])) ? esc_attr($input['style']) : 1;
-	    $valid['animation'] = (isset($input['animation']) && !empty($input['animation'])) ? esc_attr($input['animation']) : 'slide';
-	    $valid['main_container'] = (isset($input['main_container']) && !empty($input['main_container'])) ? esc_attr($input['main_container']) : '.entry-thumbnail, .entry-header, .entry-content';
+                array(
+                    'id'      => 'hide_thumbnails',
+                    'type'    => 'switcher',
+                    'title'   => esc_attr__( 'Hide thumbnails', 'exopite-lightbox35' ),
+                    'default' => 'no',
+                ),
 
-        $valid['gallery_mode'] = (isset($input['gallery_mode']) && !empty($input['gallery_mode'])) ? 1 : 0;
-        $valid['gallery_mode_container'] = (isset($input['gallery_mode_container']) && !empty($input['gallery_mode_container'])) ? esc_attr($input['gallery_mode_container']) : 'lightbox35-gallery';
+                array(
+                    'id'      => 'hide_open_in_new_window',
+                    'type'    => 'switcher',
+                    'title'   => esc_attr__( 'Hide open in new window', 'exopite-lightbox35' ),
+                    'default' => 'no',
+                ),
 
-	    return $valid;
-	}
+                array(
+                    'id'      => 'hide_download',
+                    'type'    => 'switcher',
+                    'title'   => esc_attr__( 'Hide download', 'exopite-lightbox35' ),
+                    'default' => 'no',
+                ),
 
-	public function options_update() {
-		register_setting($this->plugin_name, $this->plugin_name, array($this, 'validate'));
-	}
+                array(
+                    'id'            => 'style',
+                    'type'          => 'select',
+                    'title'         => esc_attr__( 'Caption style', 'exopite-lightbox35' ),
+                    'options'       => array(
+                        '1'             => esc_attr__( 'Inline caption', 'exopite-lightbox35' ),
+                        '2'             => esc_attr__( 'Caption under the image', 'exopite-lightbox35' ),
+                    ),
+                    'default'       => '1',
+                    'class'         => 'chosen',
+                ),
+
+                array(
+                    'id'            => 'animation',
+                    'type'          => 'select',
+                    'title'         => esc_attr__( 'Changing slide animation', 'exopite-lightbox35' ),
+                    'options'       => array(
+                        'slide'         => esc_attr__( 'Slide', 'exopite-lightbox35' ),
+                        'fade'          => esc_attr__( 'Fade', 'exopite-lightbox35' ),
+                    ),
+                    'default'       => 'slide',
+                    'class'         => 'chosen',
+                ),
+
+                array(
+                    'id'        => 'main_container',
+                    'type'      => 'text',
+                    'title'     => esc_attr__( 'Lightbox Main Container', 'exopite-lightbox35' ),
+                    'default'   => '.entry-thumbnail, .entry-header, .entry-content',
+                    'before'    => esc_attr__( 'You can specify main container here. Exopite LightBox35 looking images for lightbox effect only inside this container.', 'exopite-lightbox35' ),
+                ),
+
+                array(
+                    'id'      => 'gallery_mode',
+                    'type'    => 'switcher',
+                    'title'   => esc_attr__( 'Gallery mode', 'exopite-lightbox35' ),
+                    'default' => 'no',
+                ),
+
+                array(
+                    'id'        => 'gallery_mode_container',
+                    'type'      => 'text',
+                    'title'     => esc_attr__( 'Gallery Mode Container', 'exopite-lightbox35' ),
+                    'default'   => 'lightbox35-gallery',
+                    'before'    => esc_attr__( 'You can specify gallery mode class here. Exopite LightBox35 looking images as gallery with this class. Other images will be opened as single. Without the leading point.', 'exopite-lightbox35' ),
+                ),
+
+                array(
+                    'type'    => 'content',
+                    'content' => '<ul style="list-style: disc; padding-left: 30px; font-size: 0.8em; line-height: 1.3em; max-width: 100%;"><li><a target="_blank" href="http://wppb.io/"><b>The WordPress Plugin Boilerplate</b></a>' . esc_attr__("A standardized, organized, object-oriented foundation for building high-quality WordPress Plugins.", 'exopite-lightbox35' ) . '</li></ul>',
+                ),
+
+            ),
+        );
+
+        /**
+         * instantiate your admin page
+         */
+        $options_panel = new Exopite_Simple_Options_Framework( $config, $fields );
+
+    }
+
 
 }
